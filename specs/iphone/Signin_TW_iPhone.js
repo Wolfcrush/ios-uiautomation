982be6be982b59d501a5810d563ed4119b352d5a
Dmytro Volkodav <Dmytro@Dmytros-MacBook-Pro.local>
2013-10-28 16:39:42 -0700

    Updated

--- specs/iphone/Signin_TW_iPhone.js
+++ specs/iphone/Signin_TW_iPhone.js
@@ -34,10 +34,10 @@ app.keyboard().typeString(user1_handle);
 target.delay(2);
 Win.scrollViews()[0].webViews()[0].elements()[18].tap();
 target.delay(1);
-app.keyboard().typeString(user1_psswd);
+app.keyboard().typeString(user1_psswd+"\n");
 target.delay(1)
-Win.scrollViews()[0].webViews()[0].buttons()[0].scrollToVisible();
-Win.scrollViews()[0].webViews()[0].buttons()[0].tap();
+//Win.scrollViews()[0].webViews()[0].buttons()[0].scrollToVisible();
+//Win.scrollViews()[0].webViews()[0].buttons()[0].tap();
 Wait(app.navigationBar().rightButton(), 3);
 
 if (app.navigationBar().rightButton().checkIsValid()) {
