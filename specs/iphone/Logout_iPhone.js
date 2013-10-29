599669cea28824c528344b6706e2ee715036c588
Dmytro Volkodav <Dmytro@Dmytros-MacBook-Pro.local>
2013-10-29 14:18:23 -0700

    Test

--- specs/iphone/Logout_iPhone.js
+++ specs/iphone/Logout_iPhone.js
@@ -3,14 +3,7 @@ describe("Banjo - Logout.", function() {
 //---------------------------------------
 	it("SETTINGS.LOGOUT", function() {
 
-		app.navigationBar().leftButton().tap();
-		target.delay(1);
-		Win.tableViews()[0].cells()[5].tap();
-		Screenshot(num++, 1);
-		Win.tableViews()[1].cells()[8].tap();
-		Screenshot(num++, 1);
-		app.actionSheet().buttons()[0].tap();
-		target.delay(5);
+
 	});
 //---------------------------------------
 
